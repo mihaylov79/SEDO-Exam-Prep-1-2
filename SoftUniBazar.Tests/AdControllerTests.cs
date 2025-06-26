48c847596409bae79f864ff87ed8e5876a282908
mihaylov79 <misho.mihaylov@gmail.com>
2025-06-26 16:47:14 +0300

    test

--- SoftUniBazar.Tests/AdControllerTests.cs
+++ SoftUniBazar.Tests/AdControllerTests.cs
@@ -10,7 +10,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using SoftUniBazar.Data.Models;
-
+//TEST
 namespace SoftUniBazar.Tests
 {
     [TestFixture]
